af4440ab0527d4ee9812e1e4ab9a1cba857fd5c2
maria-alane <maria.alane.bs@gmail.com>
2022-02-09 23:28:05 -0300

    ViewBinding e data picker

---
+++ app/src/main/java/com/chaveirinho/todolist/ui/AddTaskActivity.kt
@@ -0,0 +1,38 @@
+package com.chaveirinho.todolist.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chaveirinho.todolist.databinding.ActivityAddTaskBinding
+import com.chaveirinho.todolist.extensions.format
+import com.chaveirinho.todolist.extensions.text
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.*
+
+class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    //adicionando calendario (datePicker)
+    private fun insertListeners() {
+        binding.tilDate.editText?.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
+            //adicionando função ao botão positivo do calendario
+            datePicker.addOnPositiveButtonClickListener {
+                //Usando time zone para pegar a data corretamente
+                val timeZone = TimeZone.getDefault()
+                val offSet = timeZone.getOffset(Date().time) * -1
+                binding.tilDate.text = Date(it + offSet).format()
+            }
+            datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
+    }
+
+}
