85eadd0bc205e080e535550b804c455aaf612081
maria-alane <maria.alane.bs@gmail.com>
2022-02-14 10:04:29 -0300

    Adicionando funcao edit e delete

--- app/src/main/java/com/chaveirinho/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/com/chaveirinho/todolist/ui/AddTaskActivity.kt
@@ -22,6 +22,15 @@ class AddTaskActivity : AppCompatActivity() {
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (intent.hasExtra(TASK_ID)) {
+            val taskId = intent.getIntExtra(TASK_ID, 0)
+            TaskDataSource.findById(taskId)?.let {
+                binding.tilTitulo.text = it.title
+                binding.tilDate.text = it.date
+                binding.tilHour.text = it.hour
+            }
+        }
+
         insertListeners()
     }
 
@@ -44,8 +53,9 @@ class AddTaskActivity : AppCompatActivity() {
                 .setTimeFormat(TimeFormat.CLOCK_24H)
                 .build()
             timePicker.addOnPositiveButtonClickListener {
-                val minute = if(timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
-                val hour = if(timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
+                val minute =
+                    if (timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+                val hour = if (timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
 
                 binding.tilHour.text = "$hour:$minute"
             }
@@ -60,7 +70,8 @@ class AddTaskActivity : AppCompatActivity() {
             val task = Task(
                 title = binding.tilTitulo.text,
                 date = binding.tilDate.text,
-                hour = binding.tilHour.text
+                hour = binding.tilHour.text,
+                id = intent.getIntExtra(TASK_ID, 0)
             )
 
             TaskDataSource.insertTask(task)
@@ -68,6 +79,10 @@ class AddTaskActivity : AppCompatActivity() {
             setResult(Activity.RESULT_OK)
             finish()
         }
+    }
+
+    companion object {
+        const val TASK_ID = "task_id"
     }
 
 }
